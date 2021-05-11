bfaea925af548ee28c00c0649acec7fdde5ca2b2
bpronin <boris.i.pronin@gmail.com>
2021-05-12 00:30:54 +0300

    Initial commit

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,16 +1,23 @@
+const HISTORY_PAGE_URL = chrome.runtime.getURL("history.html")
 const OPTIONS_PAGE_URL = chrome.runtime.getURL("options.html")
 
-let optionsButton = document.getElementById("options_button");
+function selectTab(tab) {
+    chrome.tabs.update(tab.id, {highlighted: true})
+}
 
-optionsButton.addEventListener("click", async () => {
-
-    chrome.tabs.query({url: OPTIONS_PAGE_URL}, tabs => {
-        let tab = tabs[0]
+function openTab(url) {
+    chrome.tabs.query({url: url}, tabs => {
+        const tab = tabs[0]
         if (tab) {
-            chrome.tabs.update(tab.id, {highlighted: true})
+            selectTab(tab)
         } else {
-            chrome.tabs.create({url: OPTIONS_PAGE_URL})
+            chrome.tabs.create({url: url})
         }
     })
+}
 
-})
+let historyButton = document.getElementById("history_button");
+let optionsButton = document.getElementById("options_button");
+
+historyButton.addEventListener("click", async () => openTab(HISTORY_PAGE_URL))
+optionsButton.addEventListener("click", async () => openTab(OPTIONS_PAGE_URL))
