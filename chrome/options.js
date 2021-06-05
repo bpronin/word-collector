773c61985c34e6117707b3dca49e25164e94a2e7
bpronin <boris.i.pronin@gmail.com>
2021-06-05 23:22:19 +0300

    refactoring

--- chrome/options.js
+++ chrome/options.js
@@ -4,8 +4,7 @@ const $spreadsheetMoreButton = $("spreadsheet_more_button")
 
 let accountLoggedIn = false
 let spreadsheetMoreSectionVisible = false
-let spreadsheetSheet
-let spreadsheetInfo
+let spreadsheetId
 
 function updateLoginSection() {
     setVisible($loginButton, !accountLoggedIn)
@@ -13,13 +12,13 @@ function updateLoginSection() {
     setVisible($("options_sections"), accountLoggedIn)
 }
 
-function updateSpreadsheetSection() {
+function updateSpreadsheetSection(spreadsheetTitle) {
     const nameLabel = $("spreadsheet_name_label")
     const idLabel = $("spreadsheet_id_label")
-    if (spreadsheetInfo) {
-        nameLabel.innerHTML = spreadsheetInfo.properties.title
-        nameLabel.setAttribute("href", spreadsheetUrl(spreadsheetInfo.spreadsheetId))
-        idLabel.innerHTML = spreadsheetInfo.spreadsheetId
+    if (spreadsheetId) {
+        nameLabel.innerHTML = spreadsheetTitle
+        nameLabel.setAttribute("href", spreadsheetUrl(spreadsheetId))
+        idLabel.innerHTML = spreadsheetId
     } else {
         nameLabel.innerHTML = ""
         nameLabel.setAttribute("href", "")
@@ -39,14 +38,12 @@ function onLoginStateChanged(loggedIn) {
 }
 
 function onCurrentSheetChanged(sheet) {
-    spreadsheetSheet = sheet
 }
 
 function onSpreadsheetInfoChanged(info) {
-    spreadsheetInfo = info
-    updateSpreadsheetSection()
+    spreadsheetId = info.spreadsheetId
+    updateSpreadsheetSection(info.properties.title)
     sendMessage(ACTION_GET_CURRENT_SPREADSHEET)
-    // sendMessage(ACTION_GET_HISTORY)
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -60,15 +57,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             case ACTION_CURRENT_SHEET_CHANGED:
                 onCurrentSheetChanged(request.data);
                 break;
-            // case ACTION_HISTORY_CHANGED:
-            // const list = $("history_list");
-            // list.innerHTML = ""
-            // for (const item of request.data) {
-            //     const row = document.createElement("div")
-            //     row.innerHTML = item
-            //     list.appendChild(row);
-            // }
-            // break
         }
         sendResponse()
     }
